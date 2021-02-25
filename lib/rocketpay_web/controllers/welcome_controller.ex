9e8c152280ee2643011709493212031394df21a2
brenoos <breno.oliver07@gmail.com>
2021-02-24 21:46:20 -0300

    first class

--- lib/rocketpay_web/controllers/welcome_controller.ex
+++ lib/rocketpay_web/controllers/welcome_controller.ex
@@ -1,7 +1,23 @@
 defmodule RocketpayWeb.WelcomeController do
   use RocketpayWeb, :controller
 
-  def index(connection, _params) do
-    text(connection, "Welcome to the Rocketpay API")
+  alias Rocketpay.Numbers
+
+  def index(connection, %{"filename" => filename }) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(connection)
+  end
+
+  defp handle_response({:ok, %{result: result}}, connection) do
+    connection
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to Rocketpay API. Here is your Number #{result}"})
+  end
+
+  defp handle_response({:error, reason}, connection) do
+    connection
+    |> put_status(:bad_request)
+    |> json(reason)
   end
 end
